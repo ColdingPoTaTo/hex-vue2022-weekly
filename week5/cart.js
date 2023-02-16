bf03652f591bd6e2bf17d453baede0a420076fc7
William <william.9150@gmail.com>
2023-02-16 23:25:31 +0800

    新增vee-validate

--- week5/cart.js
+++ week5/cart.js
@@ -1,6 +1,24 @@
-import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js';
+// import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js';
+const { createApp } = Vue;
 const apiUrl = 'https://vue3-course-api.hexschool.io/v2';
 const apiPath = 'hexpotato';
+
+const { defineRule, Form, Field, ErrorMessage, configure } = VeeValidate;
+const { required, email, min, max, digits } = VeeValidateRules;
+const { localize, loadLocaleFromURL } = VeeValidateI18n;
+
+defineRule('required', required);
+defineRule('email', email);
+defineRule('min', min);
+defineRule('max', max);
+defineRule('digits', digits);
+
+
+configure({
+    generateMessage: VeeValidateI18n.localize("zh_TW"), // 切換成中文版
+    //validateOnInput: true, // 輸入文字時，就立即進行驗證
+});
+
 
 const productModal = {
     props: ['id', 'addToCart'], // 這裡的id是從父層傳過來的，當id改變時，會觸發watch取得遠端資料，並呈現modal
@@ -119,22 +137,28 @@ const app = createApp({
                 })
         },
         onSubmit() {
-            console.log('送出表單');
+            const url = `${apiUrl}/api/${apiPath}/order`;
+            const order = this.form;
+            axios.post(url, { data: order })
+                .then((response) => {
+                    alert(response.data.message);
+                    this.$refs.form.resetForm(); // 重置表單
+                    this.getCart();
+                }).catch((err) => {
+                    alert(err.response.data.message);
+                });
+
         },
     },
     mounted() {
         this.getProduct();
         this.getCart();
     },
-    components: { productModal }
-});
-
-app.component('VForm', VeeValidate.Form);
-app.component('VField', VeeValidate.Field);
-app.component('ErrorMessage', VeeValidate.ErrorMessage);
-Object.keys(VeeValidateRules).forEach(rule => {
-    if (rule !== 'default') {
-        VeeValidate.defineRule(rule, VeeValidateRules[rule]);
+    components: {
+        productModal,
+        VForm: Form,
+        VField: Field,
+        ErrorMessage: ErrorMessage,
     }
 });
 
